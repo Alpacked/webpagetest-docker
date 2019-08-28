2951319a55a727e054d1a944ec162662bbd683ce
Diana <rabbitbax47@gmail.com>
2019-08-28 16:13:58 +0300

    test

--- script.sh
+++ script.sh
@@ -7,6 +7,6 @@ PASS=$4
 
 CONTAINER=$(docker run -d node sh -c 'while sleep 3600; do :; done')
 
-docker exec $CONTAINER /bin/sh -c "ls -la"
+docker exec $CONTAINER /bin/sh -c "/home/node/app/d_script.sh $API_KEY $URL $USERNAME $PASS"
 
-[ $? -eq 0 ] && docker stop $CONTAINER; echo true || docker stop $CONTAINER; echo false
+[ $? -eq 0 ] && docker stop $CONTAINER; exit 0 || docker stop $CONTAINER; exit 1
