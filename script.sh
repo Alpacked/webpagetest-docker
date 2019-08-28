e2014b177dd491dbd6dc98a0d8045e750745a9c4
Diana <rabbitbax47@gmail.com>
2019-08-28 19:23:43 +0300

    test

--- script.sh
+++ script.sh
@@ -10,4 +10,12 @@ CONTAINER=$(docker run -d $IMAGE sh -c 'while sleep 3600; do :; done')
 
 docker exec $CONTAINER /bin/sh -c "./d_script.sh $API_KEY $URL $USERNAME $PASS"
 
-if [ $? -eq 0 ] then docker stop $CONTAINER; echo "exit 0;"; exit 0; else docker stop $CONTAINER; echo "exit 1;"; exit 1; fi
+if [ $? -eq 0 ]; then 
+	docker stop $CONTAINER; 
+	echo "exit 0;"; 
+	exit 0; 
+else 
+	docker stop $CONTAINER; 
+	echo "exit 1;"; 
+	exit 1; 
+fi
