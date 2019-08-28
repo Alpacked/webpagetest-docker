fc4bfb079258039b605433636516e75ae5729692
Diana <rabbitbax47@gmail.com>
2019-08-28 14:16:10 +0300

    vars was added

--- script.sh
+++ script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+API_KEY=$1
+URL=$2
+USERNAME=$3
+PASS=$4
+
 CONTAINER=$(docker run -d node sh -c 'while sleep 3600; do :; done')
 
-docker exec $CONTAINER /bin/sh -c "./d_script.sh A.bfeead532859cce2692c8020852e8b79 https://stackoverflow.com/"
+docker exec $CONTAINER /bin/sh -c "./d_script.sh $API_KEY $URL $USERNAME $PASS"
 
 [ $? -eq 0 ] && docker stop $CONTAINER; echo true || docker stop $CONTAINER; echo false
